046934b67759713d3e7ae24be8ecdef87965ec5b
Phil Hendricks <pfhendri1@gmail.com>
2018-04-17 21:54:30 -0500

    added layout wrapper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Tasks from './components/tasks/tasks'
+import Layout from './containers/layout/layout'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Tasks />
+        <Layout>
+          <Tasks />
+        </Layout>
       </div>
     );
   }
