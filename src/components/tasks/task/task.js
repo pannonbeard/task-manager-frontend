c5133e796a9a649f3b23089f57ec30d6fa96c854
Phil Hendricks <pfhendri1@gmail.com>
2018-04-18 07:24:41 -0500

    Added delete button for tasks

--- src/components/tasks/task/task.js
+++ src/components/tasks/task/task.js
@@ -1,11 +1,12 @@
 import React from "react"
 import styles from './task.css'
 
-const Task = ({title, description, priority}) => (
+const Task = ({title, description, priority, remove}) => (
   <article className={styles.Article}>
     <header className={styles.Header}>
       <h1 className={styles.HeadingOne}>{title}</h1>
-      <p className={styles.Paragraph}>Urgency - 1</p>
+      <button className={[styles.Button, styles.Delete].join(' ')} onClick={remove}>X</button>
+      <p className={styles.Paragraph}>Urgency - {priority}</p>
     </header>
     <div className={styles.Body}>{description}</div>
   </article>
