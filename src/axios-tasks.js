b875f4e0939bf80c9c10b07a616a0e7e4c2cf68b
Phil Hendricks <pfhendri1@gmail.com>
2018-04-17 22:08:32 -0500

    fixed axios tasks

--- src/axios-tasks.js
+++ src/axios-tasks.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: process.env.NODE_ENV === 'development' 
     ? 'http://localhost:4000' 
-    : 'https://task-backend-staging.herokuapp.com/'
+    : 'https://task-backend-staging.herokuapp.com'
 })
 
 export default instance
